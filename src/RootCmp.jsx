f8ec7b87b1bbf498e9d6c22aa9c2827383dcbe81
unknown <aegtar88@gmail.com>
2022-05-12 20:47:41 +0300

    changed files

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,14 +1,9 @@
-import { routes } from './routes.js';
-import { BrowserRouter as Router, Routes, Switch, Route, useLocation } from 'react-router-dom';
+import { MemoryPage } from './pages/MemoryPage';
 
 export function RootCmp() {
   return (
-    <Router>
-      <Routes>
-        {routes.map((route) => (
-          <Route key={route.path} exact element={<route.component />} path={route.path} />
-        ))}
-      </Routes>
-    </Router>
+    <div>
+      <MemoryPage />
+    </div>
   );
 }
--- src/routes.js
+++
@@ -1,8 +0,0 @@
-import { MemoryPage } from './pages/MemoryPage';
-
-export const routes = [
-  {
-    path: '/MemoryPage',
-    component: MemoryPage,
-  },
-];
